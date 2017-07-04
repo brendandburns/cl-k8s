3520b5d529b2051c8742c081b822f3f2b79c6cd8
Brendan Burns <brendan.d.burns@gmail.com>
2017-07-04 07:22:48 -0700

    Update examples to include pod creation.

--- examples.lisp
+++ examples.lisp
@@ -4,9 +4,10 @@
     (call-api-with-config "/api/v1/namespaces/default" (default-config)))
 
 (defun create-namespace (name)
-    (let ((ns '((:KIND . "Namespace")
-                (:API-VERSION . "v1")
-                (:METADATA (:NAME . name)))))
+    (let ((ns (list 
+                '(:KIND . "Namespace")
+                '(:API-VERSION . "v1")
+                (cons :METADATA (cons (cons :NAME name) '())))))
         (call-api-with-config
             "/api/v1/namespaces"
             (default-config)
@@ -18,3 +19,24 @@
             (concatenate 'string "/api/v1/namespaces/" name)
             (default-config)
             :method :DELETE))
+
+(defun create-pod ()
+    (let ((pod '((:KIND . "Pod")
+                 (:API-VERSION . "v1")
+                 (:METADATA (:NAME . "nginx"))
+                 (:SPEC
+                 (:CONTAINERS
+                    ((:NAME . "nginx") (:IMAGE . "nginx")))
+                 (:RESTART-POLICY . "Always") (:TERMINATION-GRACE-PERIOD-SECONDS . 30)
+                 (:DNS-POLICY . "ClusterFirst") (:SERVICE-ACCOUNT-NAME . "default")))))
+        (call-api-with-config
+            "/api/v1/namespaces/default/pods"
+            (default-config)
+            :method :POST
+            :body pod)))
+
+(defun delete-pod (name &key (namespace "default"))
+    (call-api-with-config
+        (concatenate 'string "/api/v1/namespaces/" namespace "/pods/" name)
+        (default-config)
+        :method :DELETE))
